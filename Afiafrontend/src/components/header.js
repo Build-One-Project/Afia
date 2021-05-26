47b3cf4884f8f9f06b75f33d72d6c7467c8acdc8
SunnyInspired <sundayelesi@gmail.com>
2021-05-26 20:10:57 +0100

    Additional upadte on the header. Added menu.js and updated header.js

--- Afiafrontend/src/components/header.js
+++ Afiafrontend/src/components/header.js
@@ -1,17 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Container, Row } from 'react-bootstrap';
-import '../style.css'
+import {Col, Row } from 'react-bootstrap';
+import '../style.css';
+import Menu from './menu';
 
 function Header() {
   return (
+    <>
         <Row className="App">
         <Col xs={2}></Col>
         <Col xs={8} style={{padding:5}}>
-            <img src="/images/afia.png" alt="logo" style={{width:'100px'}} />
+        <img src="/images/afia.png" alt="logo" style={{width:'100px'}} />
         </Col>
         <Col xs={2}></Col>
         </Row>
-      
+        <Row className="App">
+        <Col xs={2}></Col>
+        <Col xs={8} style={{padding:5}}>
+        <Menu />
+        </Col>
+        <Col xs={2}></Col>
+        </Row>
+      </>
   );
 }
 
---
+++ Afiafrontend/src/components/menu.js
@@ -0,0 +1,89 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar,NavDropdown, Col, Container, Row } from 'react-bootstrap';
+
+function Menu() {
+  return (
+        <>
+        <Navbar bg="light" expand="sm" sticky="top">
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <NavDropdown title="COSMETICS" id="basic-nav-dropdown">
+          <NavDropdown.Item href="">Lotion & Shower Gel</NavDropdown.Item>
+          <NavDropdown.Item href="">Washing & cleaning liquids</NavDropdown.Item>
+          <NavDropdown.Item href="">Hair conditioners</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="">Others...</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="GROCERIES" id="basic-nav-dropdown">
+          <NavDropdown.Item href="">Cereal & Oat</NavDropdown.Item>
+          <NavDropdown.Item href="">Dairy Products & Beverages</NavDropdown.Item>
+          <NavDropdown.Item href="">Food Flour & Pasta</NavDropdown.Item>
+          <NavDropdown.Item href="">Cooking oil</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="">Others...</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="BAKERY" id="basic-nav-dropdown">
+          <NavDropdown.Item href="">Fresh Bread</NavDropdown.Item>
+          <NavDropdown.Item href="">Cookies & Pie</NavDropdown.Item>
+          <NavDropdown.Item href="">Prepared Meals</NavDropdown.Item>
+          <NavDropdown.Item href="">Cakes & Pastries</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="">Others...</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="FASHION" id="basic-nav-dropdown">
+          <NavDropdown.Item href="">Men & Female Wears</NavDropdown.Item>
+          <NavDropdown.Item href="">Perfumes & Watches</NavDropdown.Item>
+          <NavDropdown.Item href="">Children Wears</NavDropdown.Item>
+          <NavDropdown.Item href="">Accessories</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="">Others...</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="FRUITS & VEGETABLE" id="basic-nav-dropdown">
+          <NavDropdown.Item href="">Organic Fruits</NavDropdown.Item>
+          <NavDropdown.Item href="">Organic Vegetables</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="">Others...</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="DRINKS" id="basic-nav-dropdown">
+          <NavDropdown.Item href="">Juice & Milk</NavDropdown.Item>
+          <NavDropdown.Item href="">Whiskey & Wine</NavDropdown.Item>
+          <NavDropdown.Item href="">Beer & Sparkling wine</NavDropdown.Item>
+          <NavDropdown.Item href="">Minerals & Soft Drinks</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="">Others...</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="HOUSEHOLD" id="basic-nav-dropdown">
+          <NavDropdown.Item href="">Pillows and bedspread</NavDropdown.Item>
+          <NavDropdown.Item href="">Flower Vases</NavDropdown.Item>
+          <NavDropdown.Item href="">Wall clocks & hangers</NavDropdown.Item>
+          <NavDropdown.Item href="">Interiors</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="">Others...</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="BABY & TODDLER" id="basic-nav-dropdown">
+          <NavDropdown.Item href="">Milk & Cereal</NavDropdown.Item>
+          <NavDropdown.Item href="">Feeding & Bathing items</NavDropdown.Item>
+          <NavDropdown.Item href="">Baby wear & Strollers</NavDropdown.Item>
+          <NavDropdown.Item href="">Baby Toys</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="">Others...</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="FROZEN" id="basic-nav-dropdown">
+          <NavDropdown.Item href="">Chicken & Beef</NavDropdown.Item>
+          <NavDropdown.Item href="">Fish & Shrimp</NavDropdown.Item>
+          <NavDropdown.Item href="">Vegetable &  chips</NavDropdown.Item>
+          <NavDropdown.Item href="">Baby Toys</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="">Others...</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+</Navbar>
+
+        </>
+      
+  );
+}
+
+export default Menu;
