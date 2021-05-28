b57386fa1f8bf53c99358033dfb8c2582081c80e
kenneth okenwa <okenwanonsokenneth@gmail.com>
2021-05-28 08:32:38 +0100

    create a category component that display products and their information in a card

---
+++ Afiafrontend/src/components/category.js
@@ -0,0 +1,200 @@
+import Card from "react-bootstrap/Card";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
+
+function categories() {
+  return (
+    <div className="cardComponents">
+     
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wpe15f0fe7_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Cosmetics</Card.Title>
+      <Card.Text>
+      Lotion & Shower Gel
+      Washing & cleaning liquids
+      Hair conditioners & More
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wpca750c73_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Groceries</Card.Title>
+      <Card.Text>
+      Cereal & Oat
+ Dairy Products &
+ Food Flour & Pasta
+ Cooking oil & More...
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wp0b06bb2b_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Bakery</Card.Title>
+      <Card.Text>
+      Fresh Bread
+Cookies & Pie
+Prepared Meals & More..
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wp556f3207_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Fashion</Card.Title>
+      <Card.Text>
+      Men & Female Wears
+Perfumes & Watches
+Children Wears & More...
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wp769d621e_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Fluits and vegetables</Card.Title>
+      <Card.Text>
+      Organic Fruits
+Organic vegetables
+And many more..
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wpdded743a_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Beer, Wine and Drinks</Card.Title>
+      <Card.Text>
+      Juice & Milk
+Whiskey & Wine
+Beer & Sparkling wine
+And many more...
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wp5b9ac710_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Household</Card.Title>
+      <Card.Text>
+      Pillows and bedspread
+Flower Vases
+Wall clocks & hangers
+And many more..
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wp218dc988_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Baby and Toddler</Card.Title>
+      <Card.Text>
+      Milk & Cereal
+Feeding & Bathing items
+Baby wear & Strollers
+And many more
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wp5f2e2a64_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Frozen</Card.Title>
+      <Card.Text>
+      Chicken & Beef
+ Fish & Shrimp
+ Vegetable &  chips
+And many more…
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wpe828e3cb_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Furniture</Card.Title>
+      <Card.Text>
+      Sofas &  Office Tables
+Cabinets & Public address System
+ Dinning tables & More..
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://robanstores.com/wpimages/wp915c3c83_05_06.jpg" />
+    <Card.Body>
+      <Card.Title>Gym Acc and Bags</Card.Title>
+      <Card.Text>
+      Travelling bags
+ Thread Mills
+ Dumbbells & Barbells
+And many more…
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+  <Card className="class1">
+    <Card.Img variant="top" src="http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2011/11/30/1322674065664/Chefs-kitchen-kit-001.jpg" />
+    <Card.Body>
+      <Card.Title>Kitchen Equipment and Electronics</Card.Title>
+      <Card.Text>
+      Glassware
+Dinner sets
+Microwaves & Blenders
+Coolers & More…
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <button>Add to Cart</button>
+    </Card.Footer>
+  </Card>
+
+    </div>
+  );
+}
+
+export default categories;
